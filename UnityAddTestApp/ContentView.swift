1cd4438e0a604e4439ffb551649a49fd6a688efb
Julian Beck <ju-fa-beck@t-online.de>
2020-04-04 15:48:34 +0200

    Initial Commit

---
+++ UnityAddTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnityAddTestApp
+//
+//  Created by Julian Beck on 04.04.20.
+//  Copyright © 2020 Julian Beck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
